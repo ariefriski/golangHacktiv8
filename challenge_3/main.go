42d73bee165a04c9ffa380de3fdf236cc1b9a1b2
Arief <arief.riski11@gmail.com>
2023-03-13 20:39:54 +0700

    challenge_3

---
+++ challenge_3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func countMap(kata string) map[string]int {
+	hitung := make(map[string]int)
+	for _, v := range kata {
+		_, ok := hitung[string(v)]
+		if ok {
+			hitung[string(v)] += 1
+		} else {
+			hitung[string(v)] = 1
+		}
+	}
+	return hitung
+}
+
+func LoopingAndCount (word string){
+	for _,v := range word {
+		fmt.Printf("%c\n",v)
+	}
+	map1 := countMap(word)
+	fmt.Println(map1)
+}
+
+func main() {
+	const kata = "Aqu A"
+
+	LoopingAndCount(kata)
+}
